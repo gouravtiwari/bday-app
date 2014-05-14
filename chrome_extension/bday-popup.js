fcc33976dcfa43bfe4a57f2db25f4f58a889001f
Gourav Tiwari <gouravtiwari21@gmail.com>
2014-05-14 12:11:26 +0530

    refactored code and formatted UI thanks to @manishspuri :sunglasses: :+1:

--- chrome_extension/bday-popup.js
+++ chrome_extension/bday-popup.js
@@ -1,12 +1,14 @@
 // var QUERY = 'birthday';
 var date = new Date();
+var monthNames = [ "January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December" ];
 var currentMonth = date.getMonth() + 1;
 var birthdayWisher = {
   /**
    * @type {string}
    * @private
    */
-  searchOnBdayServer: 'http://localhost:1337/bday?month='+currentMonth,
+  searchOnBdayServer: 'http://172.28.228.64:1337/bday?month='+currentMonth,
 
   /**
    * Sends an XHR GET request to grab photos of lots and lots of people. The
@@ -30,31 +32,43 @@ var birthdayWisher = {
    * @private
    */
   showBdays: function (e) {
+    this.addHeader();
     var people = JSON.parse(e.target.responseText);
     for (var i = 0; i < people.length; i++) {
-      var imageEnclosingDiv = document.createElement('div');
-      imageEnclosingDiv.setAttribute('style', 'float:left');
-      imageEnclosingDiv.setAttribute('class', 'image-name');
-      
-
-      var defaultImage = document.createElement('img');
-      defaultImage.src = this.constructDefaultImageURL();
-
-      var imgObject = document.createElement('object');
-      imgObject.data = this.constructBdayURL(people[i]);
-      imgObject.setAttribute('alt', "Happy B'day "+ people[i].name + " " + people[i].day + "/" + people[i].month);
-      imgObject.setAttribute('title', "Happy B'day "+ people[i].name + " " + people[i].day + "/" + people[i].month);
-      imgObject.setAttribute('type', "image/jpg");
-
-      var nameElement = document.createElement('div');
-      var nameTextNode = document.createTextNode(people[i].name.replace(' ', '\n\n'));
-      nameElement.appendChild(nameTextNode);
-
-      imageEnclosingDiv.appendChild(imgObject).appendChild(defaultImage);
-      imageEnclosingDiv.appendChild(nameElement);
-
-      document.body.appendChild(imageEnclosingDiv);
+      this.addPicsAndName(people[i]);
     }
+  },
+
+  addHeader: function() {
+    var header = document.createElement('h3');
+    var headerTextNode = document.createTextNode('Birthdays in ' + monthNames[date.getMonth()]);
+    header.appendChild(headerTextNode);
+    document.body.appendChild(header);
+  },
+  
+  addPicsAndName: function(person){
+    var imageEnclosingDiv = document.createElement('div');
+    imageEnclosingDiv.setAttribute('style', 'float:left');
+    imageEnclosingDiv.setAttribute('class', 'image-name');
+    
+
+    var defaultImage = document.createElement('img');
+    defaultImage.src = this.constructDefaultImageURL();
+
+    var imgObject = document.createElement('object');
+    imgObject.data = this.constructBdayURL(person);
+    imgObject.setAttribute('alt', "Happy B'day "+ person.name + " " + person.day + "/" + person.month);
+    imgObject.setAttribute('title', "Happy B'day "+ person.name + " " + person.day + "/" + person.month);
+    imgObject.setAttribute('type', "image/jpg");
+
+    var nameElement = document.createElement('div');
+    var nameTextNode = document.createTextNode(person.name.replace(' ', '\n\n'));
+    nameElement.appendChild(nameTextNode);
+
+    imageEnclosingDiv.appendChild(imgObject).appendChild(defaultImage);
+    imageEnclosingDiv.appendChild(nameElement);
+
+    document.body.appendChild(imageEnclosingDiv);
   },
 
   /**
@@ -64,7 +78,7 @@ var birthdayWisher = {
    * @private
    */
   constructDefaultImageURL: function () {
-    return "http://localhost:1337/images/default.jpg";
+    return "http://172.28.228.64:1337/images/default.jpg";
   },
 
   /**
@@ -75,7 +89,7 @@ var birthdayWisher = {
    * @private
    */
   constructBdayURL: function (photo) {
-    return "http://localhost:1337/images/"+ photo.imageName;
+    return "http://172.28.228.64:1337/images/"+ photo.imageName;
   }
 };
 
