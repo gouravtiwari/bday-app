c772f79730c82ee0ae8e2fc874e6c74bf24e454b
Gourav Tiwari <gouravtiwari21@gmail.com>
2014-05-28 12:41:58 +0530

    refactored chrome extension

--- chrome_extension/bday-popup.js
+++ chrome_extension/bday-popup.js
@@ -1,14 +1,28 @@
-// var QUERY = 'birthday';
+/**
+ * get application url from menifest.json
+ * @return {string} Application URL
+ */
+function getAppURL() { 
+  console.log('here');
+  var xhr = new XMLHttpRequest();   
+  xhr.open('GET','./manifest.json',false);
+  xhr.send(null);
+
+  return JSON.parse(xhr.responseText).permissions[0];
+}
+
 var date = new Date();
 var monthNames = [ "January", "February", "March", "April", "May", "June",
     "July", "August", "September", "October", "November", "December" ];
 var currentMonth = date.getMonth() + 1;
+var appURL = getAppURL();
+
 var birthdayWisher = {
   /**
    * @type {string}
    * @private
    */
-  searchOnBdayServer: 'http://172.28.228.64:1337/bday?month='+currentMonth,
+  searchOnBdayServer: appURL+'bday?month='+currentMonth,
 
   /**
    * Sends an XHR GET request to grab photos of lots and lots of people. The
@@ -32,43 +46,74 @@ var birthdayWisher = {
    * @private
    */
   showBdays: function (e) {
-    this.addHeader();
+    document.body.appendChild(this.addHeader());
     var people = JSON.parse(e.target.responseText);
     for (var i = 0; i < people.length; i++) {
-      this.addPicsAndName(people[i]);
+      document.body.appendChild(this.personDetail(people[i]));
     }
   },
 
+  /**
+   * construct header element
+   * @return {DOMElement} Header element
+   */
   addHeader: function() {
     var header = document.createElement('h3');
     var headerTextNode = document.createTextNode('Birthdays in ' + monthNames[date.getMonth()]);
     header.appendChild(headerTextNode);
-    document.body.appendChild(header);
+    return header;
   },
   
-  addPicsAndName: function(person){
+  /**
+   * construct enclosing div element for image and name
+   * @person {object} A person
+   * @return {DOMElement} Person details with image and name elements
+   */
+  personDetail: function(person){
     var imageEnclosingDiv = document.createElement('div');
     imageEnclosingDiv.setAttribute('style', 'float:left');
     imageEnclosingDiv.setAttribute('class', 'image-name');
     
+    imageEnclosingDiv.appendChild(this.personImage(person)).appendChild(this.defaultImage());
+    imageEnclosingDiv.appendChild(this.personName(person));
 
-    var defaultImage = document.createElement('img');
-    defaultImage.src = this.constructDefaultImageURL();
+    return imageEnclosingDiv;
+  },
 
+  /**
+   * construct default image element
+   * @return {DOMElement} Default image element
+   */
+  defaultImage: function(){
+    var defaultImageElement = document.createElement('img');
+    defaultImageElement.src = this.constructDefaultImageURL();
+    return defaultImageElement;
+  },
+
+  /**
+   * construct image element for a person
+   * @person {object} A person
+   * @return {DOMElement} Image element
+   */
+  personImage: function(person){
     var imgObject = document.createElement('object');
     imgObject.data = this.constructBdayURL(person);
     imgObject.setAttribute('alt', "Happy B'day "+ person.name + " " + person.day + "/" + person.month);
     imgObject.setAttribute('title', "Happy B'day "+ person.name + " " + person.day + "/" + person.month);
     imgObject.setAttribute('type', "image/jpg");
+    return imgObject;
+  },
 
+  /**
+   * construct name element for a person
+   * @person {object} A person
+   * @return {DOMElement} Name element
+   */
+  personName: function(person){
     var nameElement = document.createElement('div');
     var nameTextNode = document.createTextNode(person.name.replace(' ', '\n\n'));
     nameElement.appendChild(nameTextNode);
-
-    imageEnclosingDiv.appendChild(imgObject).appendChild(defaultImage);
-    imageEnclosingDiv.appendChild(nameElement);
-
-    document.body.appendChild(imageEnclosingDiv);
+    return nameElement;
   },
 
   /**
@@ -78,18 +123,18 @@ var birthdayWisher = {
    * @private
    */
   constructDefaultImageURL: function () {
-    return "http://172.28.228.64:1337/images/default.jpg";
+    return appURL+"images/default.jpg";
   },
 
   /**
    * Given a photo, construct a URL using the method outlined at
    *
-   * @param {DOMElement} A person.
+   * @photo {DOMElement} A photo.
    * @return {string} The person's image URL.
    * @private
    */
   constructBdayURL: function (photo) {
-    return "http://172.28.228.64:1337/images/"+ photo.imageName;
+    return appURL+"images/"+ photo.imageName;
   }
 };
 
